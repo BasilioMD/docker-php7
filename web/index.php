0c8124c65151044f56b5285f8b973edf65d31b7c
basmendeldwm <basmendeldwm@gmail.com>
2018-10-27 22:42:19 +0200

    Agregado index

--- web/index.php
+++ web/index.php
@@ -1,21 +1,46 @@
-<?php
+<html>
+ <head>
+  <title>Bienvenido</title>
 
-$conexion = mysqli_connect( "bbdd7" , "root", "root" ) or die ( "No se ha podido conectar al servidor de Base de datos" );
-$db = mysqli_select_db( $conexion, "bbdd" ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
-$consulta = "SELECT * FROM Personas";
-$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos" );
+  <meta charset="utf-8">
 
-echo "<table borde='2'><tr><th>id</th><th>nombre</th></tr>";
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
-while ( $columna = mysqli_fetch_array( $resultado ) ){
- echo "<tr>";
- echo "<td>" . $columna['id'] . "</td><td>" . $columna['nombre'] . "</td>";
- echo "</tr>";
-}
+</head>
+<body>
+	<div class="container">
+    	<?php echo "<h1><u>Datos</u></h1>"; 
+	
+	// Conectando, seleccionando la base de datos
+	$link = mysqli_connect( 'bbdd7' , 'root' , 'root' ) 
+		or die ( 'No se pudo conectar: ' . mysqli_error() );
+	mysqli_select_db( $link, 'bbdd' ) or die( 'No se pudo seleccionar la base de datos' );
 
-echo "</table>";
+	// Realizar una query MySQL
+	$query = 'SELECT * FROM Personas';
+	$result = mysqli_query( $link, $query ) or die ( 'Consulta fallida: ' . mysqli_error()  );
 
-mysqli_close( $conexion );
+	// Imprimir los results en HTML
+	echo '<table class="table table-striped">';
+	echo '<thead><tr><th></th><th>Id</th><th>Nombre</th></tr></thead>';
+	while ( $line = mysqli_fetch_array( $result ) ){
+		echo '<tr>';
+		echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
+	    foreach ( $line as $col_value ) {
+		echo "<td>$col_value</td>";
+	    }
+	    echo "</tr>";
+	}
+	echo '</table>';
 
-?>
+	// Cerrar la conexión
+	mysqli_close( $link );
+
+	?>
+
+    </div>
+</body>
+</html>
 
